23390f9ca127349e4a6c2225ed267a727c623f17
dunahan <25346708+dunahan@users.noreply.github.com>
2021-03-17 13:12:28 +0100

    Update ccef.bat
    Added some more descriptions and the creation of necessary folders

--- batch_scripts/ccef.bat
+++ batch_scripts/ccef.bat
@@ -1,8 +1,13 @@
+@echo off
+REM build up environment
 set tempf=C:\tmp\mod\
 set moneo=C:\tmp\
-set nivto=C:\util\nwn_nim64\
-set files="C:\Users\dunahan\Documents\Neverwinter Nights\modules\moneo\"
+set nivto=C:\Users\Wirth\Documents\util\nwn_nim64\
+set files="C:\Users\Wirth\Documents\Neverwinter Nights\modules\_schwerterkueste261ee\"
 set script=msp
+
+md %tempf%
+md %moneo%chg
 
 REM copy area files
 copy /Y %files%*.are %tempf%
@@ -16,5 +21,5 @@ REM run moneo script
 %moneo%m27 %script%.ls > %script%.txt
 
 REM goto folder unpack the files from erf
-cd %moneo%chg\
-%nivto%nwn_erf -f %moneo%\mod.erf -x
+REM cd %moneo%chg\
+REM %nivto%nwn_erf -f %moneo%\mod.erf -x
