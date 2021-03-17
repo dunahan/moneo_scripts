6dd24e7c5fa217e179bf13cd4f87d8ae729211f0
dunahan <25346708+dunahan@users.noreply.github.com>
2021-03-17 11:14:46 +0100

    Update ccef.bat
    Corrected typos and added extraction

--- batch_scripts/ccef.bat
+++ batch_scripts/ccef.bat
@@ -5,12 +5,16 @@ set files="C:\Users\dunahan\Documents\Neverwinter Nights\modules\moneo\"
 set script=msp
 
 REM copy area files
-copy %files%*.are %tempf% /Y
-copy %files%*.git %tempf% /Y
-copy %files%*.gic %tempf% /Y
+copy /Y %files%*.are %tempf%
+copy /Y %files%*.git %tempf%
+copy /Y %files%*.gic %tempf%
 
 REM create erf
 %nivto%nwn_erf -c %tempf% -f %moneo%mod.erf
 
 REM run moneo script
 %moneo%m27 %script%.ls > %script%.txt
+
+REM goto folder unpack the files from erf
+cd %moneo%chg\
+%nivto%nwn_erf -f %moneo%\mod.erf -x
